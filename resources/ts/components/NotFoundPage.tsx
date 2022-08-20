52b9c2f0b6677122ed7ce87e32bb7191f3f6540e
sho0322 <infinity0322s@gmail.com>
2022-08-20 21:56:44 +0900

    add:routes

---
+++ resources/ts/components/NotFoundPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const NotFound = () => {
+    return <h2>Not Found Page</h2>;
+};
+export default NotFound;
---
+++ resources/ts/components/Richer/Renraku/Index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+    return <h1>renraku_Index</h1>;
+};
+
+export default Index;
